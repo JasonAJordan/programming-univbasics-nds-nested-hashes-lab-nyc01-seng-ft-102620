f9f22d9a3f8aa254821573546c60b6bfe445c237
Jason Jordan <jasonajordan0@gmail.com>
2020-10-02 23:34:04 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,13 +11,13 @@ def assembled_aoh
   # Array. Ruby constants are denoted by ALL_CAPS
   #arr = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
   
-  arr = [{ name:  "Don Gately", occupation: "Live-in Staff" },
+  ARR = [{ name:  "Don Gately", occupation: "Live-in Staff" },
   { name:  "Joelle van Dyne", occupation: "Radio Personality" },
   { name:  "Pat Monteseian", occupation: "Staff" },
   { name:  "Kate Gompert", occupation: "None" },
   { name:  "Bruce Green", occupation: "Fan of Mildred" }]
   
-  arr
+  ARR
 end
 
 def literal_aoh
