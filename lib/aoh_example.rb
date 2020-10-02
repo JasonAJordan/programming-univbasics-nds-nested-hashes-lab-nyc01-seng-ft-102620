5c7cce9d139110db8cf77e69a981a320a0aaf8d1
Jason Jordan <jasonajordan0@gmail.com>
2020-10-02 23:38:32 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -5,6 +5,7 @@ JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
 PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+arr =[]
 
 def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
